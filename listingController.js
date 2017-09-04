61eeb002a08c0be710576c03c67e27f36a14b9e1
cecrigope <cecrigope@hotmail.com>
2017-09-04 03:55:39 -0400

    EOD: TODO: Fix deletion index

--- listingController.js
+++ listingController.js
@@ -3,21 +3,38 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /*
-      Implement these functions in the controller to make your application function
-      as described in the assignment spec.
-     */
     $scope.addListing = function() {
-      
-    };
-    $scope.deleteListing = function(index) {
+      $scope.listings.push({
+        'code': $scope.buildingCode,
+        'name': $scope.buildingName,
+        'coordinates': {
+          'latitude': $scope.buildingCoor.latitude,
+          'longitude': $scope.buildingCoor.longitude,
+        },
+        'address': $scope.buildingAddr
+      });
 
+      $scope.buildingCode = undefined;
+      $scope.buildingName = undefined;
+      $scope.buildingCoor.latitude = undefined;
+      $scope.buildingCoor.longitude = undefined;
+      $scope.buildingAddr = undefined;
+
+      $('#addModal').modal({
+        close: true
+      });
     };
+
+    $scope.deleteListing = function(index) {
+      console.log(index);
+      $scope.listings.splice(index, 1);
+    };
+
     $scope.showDetails = function(index) {
-      $scope.code = Listings[index].code;
-      $scope.name = Listings[index].name;
-      $scope.coor = Listings[index].coordinates;
-      $scope.addr = Listings[index].address;
+      $scope.code = index.code;
+      $scope.name = index.name;
+      $scope.coor = index.coordinates;
+      $scope.addr = index.address;
     };
   }
 ]);
